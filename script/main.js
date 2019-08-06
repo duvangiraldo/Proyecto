ea5819aee462a9b628630bc02e2dbcc8981f4482
Duvan Giraldo Duque <duvan.giraldo@upb.edu.co>
2019-08-06 11:59:30 -0500

    Primeras funcionalidades en javascript

---
+++ script/main.js
@@ -0,0 +1,35 @@
+let secciones=[];
+let botones=[];
+
+window.onload=init;
+
+function init(){
+
+    
+    secciones[1]=document.getElementById("section_1");
+    secciones[2]=document.getElementById("section_2");
+    secciones[3]=document.getElementById("section_3");
+    secciones[4]=document.getElementById("section_4");
+   
+    
+
+    
+
+    botones[1]=document.getElementById("btn_1");
+    botones[2]=document.getElementById("btn_2");
+    botones[3]=document.getElementById("btn_3");
+    botones[4]=document.getElementById("btn_4");
+
+
+
+}
+
+function cambiarSeccion(target){
+
+    let id=target.id.split("_")[1];
+    secciones[id].classList.remove("ocultar");
+}
+
+
+
+
